651634c4ce9a602dfea62b52b9077ef48047a9cf
Kiyn Chin <keene.chin@gmail.com>
2022-08-17 17:54:43 -0400

    test rl policy across fingers

--- run_rl_policy_from_file.py
+++ run_rl_policy_from_file.py
@@ -8,12 +8,27 @@ import numpy as np
 tk.Tk().withdraw()
 env =  gym.make(primitives_module_env.env_name)
 policy_fname = tk.filedialog.askopenfilename()
+policy_size = policy_fname.split("_")[-1][:-4]
 policy = PPO.load(policy_fname)
 obs = env.reset()
 num_steps = 0
 # print(f"Starting at {obs}")
 start = time.time()
-with open(f"learned_task_completion_{primitives_module_env.env_name}.txt", 'w') as f:
+import os
+logdir = "policy_eval_logs"
+files = [f for f in os.listdir(logdir) if os.path.isfile(os.path.join(logdir,f))]
+lognum = 1
+for file in files:
+    file_lognum = int(file.split("_")[1])
+    if file_lognum >= lognum:
+        lognum = file_lognum + 1
+
+
+evaltype = input("What kind of finger for the policy? \nType original, silicone, thicker, or longer: ")
+trained = input("What kind of finger is being evaluated? \nType original, silicone, thicker, or longer: ")
+policy_eval_fname = f"{logdir}/log_{lognum}_{policy_size}_size_{trained}_trained_{evaltype}_eval.txt"
+print(f"Saving to {policy_eval_fname}")
+with open(policy_eval_fname, 'w') as f:
     rewards = []
     for i in range(int(input("Enter num trials:"))):
         while True:
@@ -23,7 +38,9 @@ with open(f"learned_task_completion_{primitives_module_env.env_name}.txt", 'w') 
             # action = env.action_space.sample()
             obs, reward, done, _ = env.step(action)
             rewards.append(reward)
-            if done or (time.time()-start)>3600:
+            timeout = (time.time()-start)>60
+            if done or timeout:
+                print(f"Done: {done}, Timed out:{timeout}")
                 print(f"Finished in {num_steps} steps")
                 reward_mean = np.mean(rewards)
                 f.write(f"{time.time()-start},{num_steps},{reward_mean}\n")
@@ -33,7 +50,6 @@ with open(f"learned_task_completion_{primitives_module_env.env_name}.txt", 'w') 
                 obs = env.reset()
                 break
 
-import test_random_policy
 
     
 
